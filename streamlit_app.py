c919cc46497ce93b879bce15f419349b121228c7
ehapinya <apinyasriyota@gmail.com>
2023-12-17 21:17:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
 # Define function to load and display image based on selected variables
-def display_image(variable1, variable2, variable3):
-    # Replace this with your logic to generate the image URL based on the variables
-    image_url = f"https://example.com/{variable1}/{variable2}/{variable3}.png"
-    st.image(image_url, caption="Generated Image", use_column_width=True)
+# def display_image(variable1, variable2, variable3):
+#     # Replace this with your logic to generate the image URL based on the variables
+#     image_url = f"https://example.com/{variable1}/{variable2}/{variable3}.png"
+#     st.image(image_url, caption="Generated Image", use_column_width=True)
 
 # Streamlit app layout
 st.title("Image Generator App")
-
+st.write("Hello World")
 # Dropdowns for variable selection
-variable1 = st.selectbox("Select Variable 1", ["Option1", "Option2", "Option3"])
-variable2 = st.selectbox("Select Variable 2", ["OptionA", "OptionB", "OptionC"])
-variable3 = st.selectbox("Select Variable 3", ["ChoiceX", "ChoiceY", "ChoiceZ"])
+# variable1 = st.selectbox("Select Variable 1", ["Option1", "Option2", "Option3"])
+# variable2 = st.selectbox("Select Variable 2", ["OptionA", "OptionB", "OptionC"])
+# variable3 = st.selectbox("Select Variable 3", ["ChoiceX", "ChoiceY", "ChoiceZ"])
 
-# Button to trigger image generation
-if st.button("Run"):
-    # Display the image based on the selected variables
-    display_image(variable1, variable2, variable3)
+# # Button to trigger image generation
+# if st.button("Run"):
+#     # Display the image based on the selected variables
+#     display_image(variable1, variable2, variable3)
